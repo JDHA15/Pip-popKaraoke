b426a5a26ebf882df8981f5f27b5ce63894786e9
LAPT-CSH-13 <cshtd.computacion@gmail.com>
2025-03-04 09:31:26 -0600

    codigo final

---
+++ main.js
@@ -0,0 +1,35 @@
+//Conexión con HTML (Seleccionar elementos)
+const songForm = document.getElementById('song_form');
+const playList = document.getElementById('play_list');
+
+//función para agregar una canción a la lista
+
+function addSong(songName, artistName, songUrl) {
+    const listSong = document.createElement('li');
+    listSong.innerHTML = `
+            <strong>${songName}</strong>
+            -${artistName}
+            <a href="${songUrl}" target ="_blank">Reproducir</a>
+            <button class="delete-btn">Eliminar</button>
+    
+    `;
+    //Agregar al HTMl
+    playList.appendChild(listSong);
+
+
+}
+
+//Manejo del envio de forumularios (Eventos)
+
+songForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    //Obtener datos del fromulario
+    const songName = document.getElementById('song_name').value;
+    const artistNanme = document.getElementById('artist_name').value;
+    const songUrl = document.getElementById('url_song').value;
+
+    addSong(songName,artistNanme, songUrl)
+
+    songForm.reset();
+} )
